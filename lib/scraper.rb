4832205f32c034e68e500a5beab1736d6bdf3c7f
Sara Nolen <nolensara1@gmail.com>
2021-03-21 16:29:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,8 +20,8 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     studnet = {}
-    profile_page = Nokogiri::HTML(open(profile_url)
-    links = profile_page.css(".social-icon-container").children.css('a').map{|el| el.attribute('href').value}
+    profile_page = Nokogiri::HTML(open(profile_url))
+    links = profile_page.css(".social-icon-container").children.css('a').map{|x| x.attribute('href').value}
       links each do |link|
         if link.include?("linkedin")
           student[:linkedin] = link
