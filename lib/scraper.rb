ad60eb182a515e665e6749884cc71432989b3a18
Sara Nolen <nolensara1@gmail.com>
2021-03-21 16:18:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,13 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     page = Nokogiri::HTML(open(index_url))
+    studnets =[]
+    page.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_name = student.css('.student-name').text
+        studnet_location = student.css('.student-location').text
+        student_profile_link = "#{{student.attr('href')}}"
+        students<<{location:student_location, name:studnet_name, profile_url:student_profile_link}
 
   end
 
