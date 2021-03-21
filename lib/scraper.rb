a4488610c10c0ac06271361d735b5eb8d179cee9
Sara Nolen <nolensara1@gmail.com>
2021-03-21 16:13:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,12 +5,11 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     page = Nokogiri::HTML(open(index_url))
-    
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
